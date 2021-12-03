c79efa2ba9ea4fdcf139d2054975425cac42c34c
yoavjs <53790416+yoavjs@users.noreply.github.com>
2021-12-03 17:11:38 +0200

    first commit

---
+++ build/CMakeFiles/LIOR.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LIOR.dir/main.cpp.obj"
+  "CMakeFiles/LIOR.dir/main.cpp.obj.d"
+  "LIOR.exe"
+  "LIOR.exe.manifest"
+  "LIOR.pdb"
+  "libLIOR.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LIOR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Projects/LearningCMake/LiorsHelp
+# Build directory: D:/Projects/LearningCMake/LiorsHelp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
